f123e7882e9a6b091b19192a7f9d56e662b59a8d
Tntmod54321 <45548254+tntmod54321@users.noreply.github.com>
2022-10-19 17:29:41 -0400

    only use zdb -R to read *damaged* blocks

--- zdb_recover.py
+++ zdb_recover.py
@@ -84,11 +84,6 @@ def getObjBlkPointers(path):
     if tsize==None: raise Exception('Unable to get total filesize')
     return LX_POINTERS, path['mntPoint'], tsize
 
-def resetZBDZLE(ZDB_NO_ZLE_default):
-    if not ZDB_NO_ZLE_default:
-        print('resetting zbd_no_zle env variable to false')
-        os.environ['ZDB_NO_ZLE'] = 'false'
-
 def main():
     # zdb -U /data/zfs/zpool.cache -R Boys 0:156ecb3f5000:20000L/8000P:rd > /mnt/Boys/audrey/testing/browserhist_l0_0_raw1_2.txt
     # python /mnt/Boys/audrey/testing/zdb_recover.py -i "/mnt/Boys/audrey/2022.09.15 centbrowser history" -o "/mnt/Boys/audrey/testing/dump.bin"
@@ -96,8 +91,6 @@ def main():
     path = ''
     outfile = ''
     overwrite = False
-    ZDB_NO_ZLE = False # if specified set env var true while running and if excepted then set back to false
-    ZDB_NO_ZLE_default = os.getenv('ZDB_NO_ZLE', default=False)
     
     if len(sys.argv[1:]) == 0: printHelp()
     i=1 # for arguments like [--command value] get the value after the command
@@ -106,7 +99,6 @@ def main():
         if (arg in ["-i", "--in-file"]): path = sys.argv[1:][i]
         if (arg in ["-o", "--out-file"]): outfile = sys.argv[1:][i]
         if (arg in ["-X"]): overwrite = True
-        if (arg in ["--zdb-no-zle"]): ZDB_NO_ZLE = True
         i+=1
     if '' in [path, outfile]: printHelp()
     
@@ -115,59 +107,54 @@ def main():
     pathExists = os.path.exists(outfile)
     if not overwrite and pathExists: raise Exception(f'Specified output file {outfile} already exists!')
     if not pathExists and not os.path.exists(os.path.split(outfile)[0]): os.makedirs(os.path.split(outfile)[0])
-    try:
-        if ZDB_NO_ZLE: os.environ['ZDB_NO_ZLE'] = 'true'
-        with open(outfile, 'wb') as f:
+    
+    with open(outfile, 'wb') as OF:
+        with open(path, 'rb') as IF:
             previousOffset=None
             i=1
             byteCounter=0
+            badBlockCounter=0
             for pointer in pointers:
                 if pointer['level'] != 'L0': continue # skip non-data pointers
                 if previousOffset!=None: # check previous offset
                     if not int(pointer['fileoffset'], 16)>previousOffset:
-                        raise Exception('Error, object block pointers being read out of order, not possible to properly reconstruct file') # it is possible you liar!!!     just not implemented 👉👈
+                        raise Exception('Error, object block pointers being read out of order, not possible to properly reconstruct file') # it is possible you liar!!!     just not implemented.. 👉👈
                 
-                # form command to read block
-                command = 'zdb '
-                if isTruenas: command+='-U /data/zfs/zpool.cache ' # non boot-pool pools are cached here
-                command+=f'-R {mntPoint} '
-                command+=f"{pointer['vdev']}:{pointer['offset']}:{pointer['size']}:r"
-                
-                if pointer['isCompressed']==True: command+='d' # add decompress flag if compression is detected in this block
-                elif pointer['isCompressed']==False: pass
-                elif pointer['isCompressed']==None: raise Exception('undefined block physical/logical bytes ratio?')
-                
-                blockRead = os.popen(command) # read block from disk
-                blockBin = blockRead.buffer.read() # read the underlying buffer that the textiowrapper is using so we don't have issues encoding to bytes 😏
-                blockReadStatus = blockRead.close()
-                if blockReadStatus!=None: raise Exception(f"Error reading block {pointer['offset']} content")
-                
-                with open(path, 'rb') as IF:
+                try:
+                    if i==2: raise OSError
                     IF.seek(byteCounter)
                     blockLen = re.match(r'^(\d{1,10})L', pointer['size'])
                     if blockLen: blockLen=int(blockLen[1], 16)
                     else: raise Exception('Error getting logical length of block')
-                    blockBin2 = IF.read(blockLen)
+                    blockBin = IF.read(blockLen)
+                except OSError as e:
+                    if not 'Input/output error' in str(e): raise e
+                    badBlockCounter+=1
+                    print(f'block {i} is damaged')
                     
-                if not blockBin2[:tsize-byteCounter]==blockBin[:tsize-byteCounter]: raise Exception('Why tf aren\'t f.read and zdb -R the same??')
+                    # form command to read block
+                    command = 'zdb '
+                    if isTruenas: command+='-U /data/zfs/zpool.cache ' # non boot-pool pools are cached here
+                    command+=f'-R {mntPoint} '
+                    command+=f"{pointer['vdev']}:{pointer['offset']}:{pointer['size']}:r"
+                    
+                    if pointer['isCompressed']==True: command+='d' # add decompress flag if compression is detected in this block
+                    elif pointer['isCompressed']==False: pass
+                    elif pointer['isCompressed']==None: raise Exception('undefined block physical/logical bytes ratio?')
+                    
+                    blockRead = os.popen(command) # read block from disk
+                    blockBin = blockRead.buffer.read() # read the underlying buffer that the textiowrapper is using so we don't have issues encoding to bytes 😏
+                    blockReadStatus = blockRead.close()
+                    if blockReadStatus!=None: raise Exception(f"Error reading block {pointer['offset']} content")
                 
-                if i==2:
-                    f.write(blockBin[:tsize-byteCounter])
-                    byteCounter+=len(blockBin[:tsize-byteCounter]) # count bytes excluding overshoot no the last block
-                else:
-                    f.write(blockBin2[:tsize-byteCounter])
-                    byteCounter+=len(blockBin2[:tsize-byteCounter]) # count bytes excluding overshoot no the last block
+                OF.write(blockBin[:tsize-byteCounter])
+                byteCounter+=len(blockBin[:tsize-byteCounter]) # count bytes excluding overshoot no the last block
                 
                 print(f"({i}/{len(pointers)-1}) read {byteCounter} bytes total")
                 
                 previousOffset = int(pointer['fileoffset'], 16)
                 i+=1
-    except Exception as e: # intended mainly for kb interrupt, but wanna reset env var anyway.
-        print('Excepted!')
-        resetZBDZLE(ZDB_NO_ZLE_default)
-        raise e
-    resetZBDZLE(ZDB_NO_ZLE_default)
-    print(f'Successfilly transfered {os.path.split(path)[1]}! ({byteCounter} bytes)')
+    print(f'Successfilly transfered {os.path.split(path)[1]}! ({byteCounter} bytes, {badBlockCounter} damaged blocks)')
 
 if __name__=='__main__':
     main()
@@ -176,5 +163,3 @@ if __name__=='__main__':
 # filesystem compression is not global (detect using ratio of physical/logical bytes)
 # the last block had extra data (count written bytes and slice output by bytecount of original file)
 # reading the binary from os.popen() (subprocess.popen didn't behave how I expected either..) (op.popen().buffer returns underlying binary buffer that's feeding the textiowrapper)
-
-# in theory you could use `dd` to do this at near full speed, reading the specified amount of logical bytes, simply retrying bad blocks using zdb, but I don't have enough data for it to be worth my time to program that
